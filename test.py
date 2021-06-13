2d3d05c8fadf723bc73b3d80783d23a367586b49
Thomkraft <69580958+Thomkraft@users.noreply.github.com>
2021-06-13 23:02:05 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-test = "test ptn "
-list = [0,0,0,0,0,0,0,0,0]
-pickle.dump(test, open("variables.txt", "wb"))
-variable = pickle.load(open("variables.txt", "rb"))
-
-print(variable)
-
