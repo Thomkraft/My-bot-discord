0a1a1b83ff78338864d04b91446599a757b83772
thomkraft <bs.petitcompte@gmail.com>
2021-06-14 07:52:38 +0200

    test

--- Stats skywars/Stats skywars.py
+++ Stats skywars/Stats skywars.py
@@ -4,7 +4,7 @@ import os
 import random
 from discord.ext import commands, tasks
 #a
-
+#bb
 bot = commands.Bot(command_prefix="/", description="Bot de Thomkraft !")
 
 botgame = ["Bot de Thomkraft !",
